e7e3cd2d7e052c8681e0fcbccf3149c65db9d8bd
Rami Gökhan Kıcı <gokhankici@gmail.com>
2017-10-31 00:31:17 -0700

    explicit arg parsing + better search queries

--- fetch-server/fetch.py
+++ fetch-server/fetch.py
@@ -34,13 +34,14 @@ def index():
 
 # ------------------------------------------------------------------------------
 
-parser = reqparse.RequestParser()
-parser.add_argument('id'             , type=int)
-parser.add_argument('name'           , type=str)
-parser.add_argument('is_shade'       , type=bool)
-parser.add_argument('is_water'       , type=bool)
-parser.add_argument('is_garbage_can' , type=bool)
-parser.add_argument('is_poop_bag'    , type=bool)
+parser = reqparse.RequestParser(trim=True, bundle_errors=True)
+parser.add_argument('id'             , type=int,  store_missing=False)
+parser.add_argument('name'           , type=str,  store_missing=False)
+parser.add_argument('address'        , type=str,  store_missing=False)
+parser.add_argument('is_shade'       , type=bool, store_missing=False)
+parser.add_argument('is_water'       , type=bool, store_missing=False)
+parser.add_argument('is_garbage_can' , type=bool, store_missing=False)
+parser.add_argument('is_poop_bag'    , type=bool, store_missing=False)
 
 route_fields = {
     'id'             : fields.Integer,
@@ -53,12 +54,24 @@ route_fields = {
 }
 
 class RouteSearch(Resource):
-    @marshal_with(route_fields)
+    @marshal_with(route_fields, envelope='results')
     def get(self):
-        args = parser.parse_args()
-        if args['id'] is None:
-            return Route.query.all()
+        args = parser.parse_args(strict=True)
+        print args
+        if 'id' not in args:
+            q = Route.query
+            if 'name' in args:
+                name = args['name']
+                args.pop('name')
+                q = q.filter(Route.name.ilike('%'+name+'%'))
+            if 'address' in args:
+                address = args['address']
+                args.pop('address')
+                q = q.filter(Route.address.ilike('%'+address+'%'))
+            return q.filter_by(**args).all()
         else:
-            return Route.query.get(args['id'])
+            route = Route.query.get(args['id'])
+            return [route] if route is not None else []
 
 api.add_resource(RouteSearch, '/api/search')
+
