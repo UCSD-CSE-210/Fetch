1a145b5179381935a533fc6716ab256d97ae9765
Rami Gökhan Kıcı <gokhankici@gmail.com>
2017-12-04 12:44:33 -0800

    added unit tests for search api

--- fetch-server/test/test_route.py
+++ fetch-server/test/test_route.py
@@ -17,6 +17,11 @@ except ValueError:
     
 srid = utils.get_default_srid()
 
+def merge_dicts(x, y):
+    z = x.copy()   # start with x's keys and values
+    z.update(y)    # modifies z with y's keys and values & returns None
+    return z
+
 class RouteTestCase(FetchTestCase):
   def setUp(self):
     super(RouteTestCase, self).setUp()
@@ -26,36 +31,83 @@ class RouteTestCase(FetchTestCase):
                              'longitude' : -117.233322}
     self.petco_latlong    = {'latitude'  :   32.869370,
                              'longitude' : -117.231178}
-
   def test_empty(self):
     with self.app.app_context():
-      self.assertEqual(len(self.manager.search({})), 9)
-  
-  # TODO: validate these orderings...
-  def test_ordering(self):
-    routes = self.manager.search(self.ucsd_cse_latlong)
-    names  = ["UCSD", 
-              "Par Course Trails", 
-              "Torrey Pines", 
-              "La Jolla Shores", 
-              "Rose Canyon", 
-              "Deerfield loop",
-              "Mission Trails Visitor Loop",
-              "Balboa Park",
-              "Cowles Mountain Trail"] 
-    self.assertEqual(map(lambda r: r.name, routes), names)
+      rs = self.manager.search({})
+      self.assertEqual(len(rs), 9)
 
-    routes2 = self.manager.search(self.petco_latlong)
-    names2  = ["UCSD", 
-               "Rose Canyon",
+      names = ["UCSD", 
                "Par Course Trails", 
-               "La Jolla Shores", 
                "Torrey Pines", 
-               "Balboa Park", 
-               "Deerfield loop", 
-               "Mission Trails Visitor Loop", 
+               "La Jolla Shores", 
+               "Rose Canyon", 
+               "Deerfield loop",
+               "Mission Trails Visitor Loop",
+               "Balboa Park",
                "Cowles Mountain Trail"]
-    self.assertEqual(map(lambda r: r.name, routes2), names2)
+
+      for r in rs:
+        self.assertTrue(r.name in names)
+  
+  def test_ordering(self):
+    with self.app.app_context():
+      routes = self.manager.search(self.ucsd_cse_latlong)
+      names  = ["UCSD", 
+                "Par Course Trails", 
+                "Torrey Pines", 
+                "La Jolla Shores", 
+                "Rose Canyon", 
+                "Deerfield loop",
+                "Mission Trails Visitor Loop",
+                "Balboa Park",
+                "Cowles Mountain Trail"] 
+      self.assertEqual(map(lambda r: r.name, routes), names)
+
+      routes2 = self.manager.search(self.petco_latlong)
+      names2  = ["UCSD", 
+                 "Rose Canyon",
+                 "Par Course Trails", 
+                 "La Jolla Shores", 
+                 "Torrey Pines", 
+                 "Balboa Park", 
+                 "Deerfield loop", 
+                 "Mission Trails Visitor Loop", 
+                 "Cowles Mountain Trail"]
+      self.assertEqual(map(lambda r: r.name, routes2), names2)
+      
+  def radius_tester(self, latlong, radius, names):
+    with self.app.app_context():
+      q  = merge_dicts(latlong, {"radius" : radius})
+      rs = self.manager.search(q)
+      self.assertEqual(map(lambda r: r.name, rs), names)
+    
+  def test_radius_ucsd_1(self):
+    self.radius_tester(self.ucsd_cse_latlong,
+                       1,
+                       ["UCSD", "Par Course Trails"])
+
+  def test_radius_ucsd_2(self):
+    self.radius_tester(self.ucsd_cse_latlong,
+                       2,
+                       ["UCSD", 
+                        "Par Course Trails", 
+                        "Torrey Pines", 
+                        "La Jolla Shores", 
+                        "Rose Canyon"])
+
+  def test_radius_petco_1(self):
+    self.radius_tester(self.petco_latlong,
+                       1,
+                       ["UCSD", "Rose Canyon"])
+
+  def test_radius_petco_2(self):
+    self.radius_tester(self.petco_latlong,
+                       2,
+                       ["UCSD", 
+                        "Rose Canyon",
+                        "Par Course Trails", 
+                        "La Jolla Shores", 
+                        "Torrey Pines"])
 
   def add_routes(self):
     # insert the surface types
